53b1a9bcfa1ae4b17075407327079dd7057006a4
Michael Kim <michael@minogram.com>
2023-08-25 20:01:58 +0900

    .

---
+++ src/Mino.VideoToSketch/Views/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mino.VideoToSketch.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Wpf.Ui.Appearance.Theme.Apply(
+           Wpf.Ui.Appearance.ThemeType.Dark,     // Theme type
+           Wpf.Ui.Appearance.BackgroundType.Mica, // Background type
+           true);
+
+            this.Loaded += (sender, args) =>
+            {
+                Wpf.Ui.Appearance.Watcher.Watch(
+                    this,                                  // Window class
+                    Wpf.Ui.Appearance.BackgroundType.Mica, // Background type
+                    true                                   // Whether to change accents automatically
+                );
+            };
+        }
+    }
+}
